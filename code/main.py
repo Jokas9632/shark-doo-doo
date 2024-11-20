c951abd88e3a4e375f73b91c53bd11fcc346aa4b
Martynas <m.kulys@student.tue.nl>
2024-11-20 13:56:45 +0100

    Remove txt to csv conversion

--- code/main.py
+++ code/main.py
@@ -3,48 +3,14 @@ import pandas as pd
 
 
 def read_activity_data():
-    with open(r"data\activityDat.csv") as file:
+    with open("cleaned_data.csv") as file:
         data = list(csv.reader(file))
-        df = pd.DataFrame(data[1:], columns=data[0])
-        df.columns = ['Year', 'Status', 'Provocation', 'Activity']
-        print(df.head())
-        return df
+        df_cleaned = pd.DataFrame(data[1:], columns=data[0])
+        df_cleaned.columns = ['Year', 'Status', 'Provocation', 'Activity', 'Day', 'Month', 'Injury', 'State',
+                              'Location', 'Latitude', 'Longitude', 'SharkName', 'SharkLength', 'SharksCount',
+                              'InjuryLocation', 'Severity', 'Gender', 'Age', 'IncidentTime', 'Latitude_timedb2',
+                              'SharkScientific', 'TimeOfDay']
+        return df_cleaned
 
 
-def read_injury_data():
-    with open(r"data\injurydat.csv") as file:
-        data = list(csv.reader(file))
-        df = pd.DataFrame(data[1:], columns=data[0])
-        df.columns = ['Day', 'Month', 'Year', 'Injury', 'State', 'Location', 'Latitude', 'Longitude', 'SharkName',
-                      'SharkLength', 'Provocation', 'SharksCount', 'Activity', 'InjuryLocation', 'Severity', 'Gender',
-                      'Age', 'IncidentTime']
-        print(df.head())
-        return df
-
-
-def read_timedb2_data():
-    with open(r"data\timedb2.csv") as file:
-        data = list(csv.reader(file))
-        df = pd.DataFrame(data[1:], columns=data[0])
-        df.columns = ['Day', 'Month', 'Year', 'Latitude', 'Longitude', 'SharkName', 'SharkScientific',
-                      'Provocation', 'Activity', 'InjuryLocation', 'InjuryDescription', 'Severity',
-                      'Gender', 'Age', 'IncidentTime']
-        print(df.head())
-        return df
-
-def read_activity_data():
-    with open(r"C:\Users\20231596\shark-doo-doo\code\cleaned_data.csv") as file:
-        data = list(csv.reader(file))
-        df = pd.DataFrame(data[1:], columns=data[0])
-        df.columns =['Year','Status','Provocation','Activity','Day','Month','Injury','State','Location','Latitude','Longitude','SharkName',
-                     'SharkLength','SharksCount','InjuryLocation','Severity','Gender','Age','IncidentTime','Latitude_timedb2','SharkScientific',
-                     'TimeOfDay']
-        print(df.head())
-        return df
-    
-
-
-df_act = read_activity_data()
-df_inj = read_injury_data()
-df_time = read_timedb2_data()
-df_final = read_activity_data()
+df = read_activity_data()
